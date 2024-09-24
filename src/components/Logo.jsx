1820209969eae8afbee60d412cb5bedce5263c7c
unknown <JhojamCaraballo@gmail.com>
2024-09-24 16:01:56 -0500

    Se Sube Login No Terminado

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import logo from '../images/logo-foot.png';
+
+function Logo() {
+    return (
+        <div className="logo">
+            <img src={logo} alt="Logo" className="logo" />
+        </div>
+    );
+}
+
+export default Logo;
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Login from './menus/Login.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Login />
+  </StrictMode>,
+)
